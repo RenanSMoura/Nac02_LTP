84448b565eceaa0c4b6735c154947a8cca560eec
leonardosouza <leonardo.souza@vivareal.com>
2015-09-30 20:32:19 -0300

    servlet main

---
+++ src/br/com/fiap/ltp/nac02/questao1/servlets/Main.java
@@ -0,0 +1,32 @@
+package br.com.fiap.ltp.nac02.questao1.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Main
+ */
+@WebServlet("/")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Main() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.sendRedirect("lista");
+	}
+}
